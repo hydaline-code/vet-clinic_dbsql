17a3564beb50ba2b08947ca486bf461c8e8a894d
hydaline-code <djougancharlen@.com>
2023-11-04 13:55:00 +0100

    Update data and schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,5 @@
 /* Database schema to keep the structure of entire database. */
-CREATE DATABASE vet_clinic;
-CREATE TABLE animals (
-  id  INTEGER,
-  name VARCHAR(255),
-  date_of_birth DATE,
-  escape_attempts INTEGER,
-  neutered BOOLEAN,
-  weight_kg DECIMAL(10, 2)
-);
+
+ALTER TABLE animals
+ADD  species VARCHAR(255) ;
+
