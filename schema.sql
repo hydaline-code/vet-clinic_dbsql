832f8199bcb1ca6ec27cceec8cf576542c639315
hydaline-code <djougancharlen@.com>
2023-11-04 16:47:42 +0100

    Add new table with primary keys

--- schema.sql
+++ schema.sql
@@ -1,5 +1,21 @@
 /* Database schema to keep the structure of entire database. */
 
+-- ALTER TABLE animals
+-- ADD  species VARCHAR(255) ;
+
+-- CREATE TABLE owners (
+--     id SERIAL PRIMARY KEY,
+--     full_name VARCHAR(255),
+--     age INTEGER
+-- );
+
+-- CREATE TABLE species (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(255)
+-- );
+
 ALTER TABLE animals
-ADD  species VARCHAR(255) ;
+ADD COLUMN species_id INTEGER REFERENCES species(id),
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
 
